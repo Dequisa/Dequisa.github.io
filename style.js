a62b0c9312716477bd3e2d6b316cdef617b4a04b
Dequisa <Dequisas@gmail.com>
2021-06-03 15:31:04 -0400

    latest changes

--- style.js
+++ style.js
@@ -1,35 +1,14 @@
 
 console.log("good luck")
-window.onscroll = function() {myFunction()};
-var header = document.querySelector(".info");
-var sticky = header.offsetTop;
-
-
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-      header.classList.add("sticky");
-    } else {
-      header.classList.remove("sticky");
-    }
-  }
+const ScrollReveal = require('scrollreveal')
+import ScrollReveal from 'scrollreveal'
 
 
 
 
+// const ul =document.getElementById("notes");
+// const li createElement('li')
+// li.textContent =
 
 
 
-
-
-
-
-// function viewPoint() {
-
-//     document.getElementById("#home").style.display = "block";
-//     document.getElementById("#me").style.display = "none";
-//     document.getElementById("#experience").style.display = "none";
-//     document.getElementById("#project").style.display = "none";
-//     document.getElementById("#container").style.display = "none";
-//     document.getElementById("#contact").style.display = "none";
-
-// }
